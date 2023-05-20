c5494fd8b2ef00c2c6d6abe7780f04bc5b9620f4
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-21 00:39:21 +0530

    added routes for the components

---
+++ folder-wizard/src/routes/route.svelte
@@ -0,0 +1,12 @@
+<script>
+
+import {Router,Route} from 'svelte-routing';
+import Login from '../components/login.svelte';
+import Signup from '../components/signup.svelte';
+
+</script>
+
+<Router>
+    <Route path="/" component={Login}/>
+    <Route path="/signup" component={Signup}/> 
+</Router>
