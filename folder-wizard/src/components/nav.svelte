0ff1123ffd6f857016df89861232e811de2b731a
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-21 13:01:42 +0530

    updated nav bar

---
+++ folder-wizard/src/components/nav.svelte
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-light bg-success">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="#">Folder Wizard</a>
+    <button type="button" class="btn btn-light">Logout</button>
+  </div>
+</nav>
