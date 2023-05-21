86ca063b7cdb92c19bf7127e23f5017525330d55
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-21 16:34:05 +0530

    crated enum for col defs

---
+++ folder-wizard/src/shared/enum/col-defs.enum.ts
@@ -0,0 +1,16 @@
+export enum COLUMN_DEFS {
+  FOLDER_NAME = "Folder Name",
+  LEVEL = "Level",
+  CATEGROY = "Category",
+  CREATED = "Created",
+}
+
+export enum CATEGORY {
+  FILE = "File",
+  FOLDER = "Folder",
+}
+
+export enum LEVEL {
+  ROOT = "root",
+  CHILD = "child",
+}
