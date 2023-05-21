bd0451d29458ac84b41b974582f40b241f83369d
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-21 14:41:28 +0530

    rename index from route

--- folder-wizard/src/routes/index.svelte
+++ folder-wizard/src/routes/index.svelte
@@ -3,10 +3,12 @@
 import {Router,Route} from 'svelte-routing';
 import Login from '../components/login.svelte';
 import Signup from '../components/signup.svelte';
+import FolderListing from '../components/folder-listing.svelte';
 
 </script>
 
 <Router>
-    <Route path="/" component={Login}/>
+    <Route path="/" component={FolderListing}/>
+    <Route path="/login" component={Login}/>
     <Route path="/signup" component={Signup}/> 
 </Router>
