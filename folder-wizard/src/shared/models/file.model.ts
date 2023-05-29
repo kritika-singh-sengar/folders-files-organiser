794d6bddfb7d6d3178ea122a637000d53abc327c
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-29 11:42:54 +0530

    made category and level attribute optinal

--- folder-wizard/src/shared/models/file.model.ts
+++ folder-wizard/src/shared/models/file.model.ts
@@ -1,7 +1,7 @@
 export interface FILE {
     name: string;
-    category: string;
-    level: string;
+    category?: string;
+    level?: string;
     parent?: string;
     rootFolder?: string;
     createdAt: string;
