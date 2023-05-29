dfd82bff89c562162bff47759b1c2832229d5098
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-30 00:03:24 +0530

    created file comonent

---
+++ folder-wizard/src/components/file.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { FILE } from "../shared/models/file.model";
+
+  let file: FILE = {
+    name: "--",
+    createdAt: "--",
+  };
+</script>
+
+<div class="card bg-light rounded-0">
+  <div class="card-body row p-2">
+    <span class="col">Name : {file.name}</span>
+    <span class="col">Created At : {file.name}</span>
+  </div>
+</div>
