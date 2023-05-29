0b91cf09b286b80b470ba54e91db614ac17e66a4
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-30 00:12:01 +0530

    added function for logout and signup and styling as well

--- folder-wizard/src/components/nav.svelte
+++ folder-wizard/src/components/nav.svelte
@@ -1,6 +1,26 @@
-<nav class="navbar navbar-light bg-success">
+<script lang="ts">
+  import { loggedInUser } from "../shared/stores/user.store";
+  import { navigate } from "svelte-routing";
+  function logout() {
+    loggedInUser.set('');
+    navigate('/login');
+  }
+
+  const signUp = ()=>{
+    navigate("/signup");
+  }
+
+</script>
+<nav class="navbar navbar-light bg-success bg-gradient text-white">
   <div class="container-fluid">
     <a class="navbar-brand text-white" href="#">Folder Wizard</a>
-    <button type="button" class="btn btn-light">Logout</button>
+    {#if $loggedInUser}
+    <span>
+      <span class="p-1 fw-bold ">{$loggedInUser}</span>
+      <button type="button" class="btn btn-light" on:click={logout}>Logout</button>
+    </span>
+    {:else}
+    <button type="button" class="btn btn-light" on:click={signUp}>Sign up</button>
+    {/if}
   </div>
 </nav>
