edac50589f6b69b20bbc9943c22a51d972595e8e
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-30 00:10:23 +0530

    make route protected

--- folder-wizard/src/routes/index.svelte
+++ folder-wizard/src/routes/index.svelte
@@ -1,14 +1,23 @@
 <script>
+  import { Router, Route } from "svelte-routing";
+  import Login from "../components/login.svelte";
+  import Signup from "../components/signup.svelte";
+  import Hierachy from "../components/hierachy.svelte";
+  import { loggedInUser } from "../shared/stores/user.store";
+  import PageNotFound from "../components/pageNotFound.svelte";
+  import AccessDenied from "../components/accessDenied.svelte";
 
-import {Router,Route} from 'svelte-routing';
-import Login from '../components/login.svelte';
-import Signup from '../components/signup.svelte';
-import FolderListing from '../components/folder-listing.svelte';
-
+  $: isAuthenticated = $loggedInUser;
 </script>
 
 <Router>
-    <Route path="/" component={FolderListing}/>
-    <Route path="/login" component={Login}/>
-    <Route path="/signup" component={Signup}/> 
+  {#if isAuthenticated}
+    <Route path="/" component={Hierachy} />
+    <!-- <Route path="/creator" component={}/>  -->
+  {:else}
+    <Route path="/signup" component={Signup} />
+    <Route path="/" component={Login} />
+  {/if}
+  <Route path="/accessDenied" component={AccessDenied} />
+  <Route component={PageNotFound} />
 </Router>
