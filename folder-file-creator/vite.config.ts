74adc0b5399ec88eb8ee6145fe70e94b140084d3
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-06-01 23:46:10 +0530

    configure for creating named js file during build

--- folder-file-creator/vite.config.ts
+++ folder-file-creator/vite.config.ts
@@ -3,6 +3,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/main.ts',
+      name: 'folder-file-creator'
+    }
+  },
   plugins: [svelte({
     compilerOptions:{
       customElement: true,
