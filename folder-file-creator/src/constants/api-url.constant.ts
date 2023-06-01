dc68e70b68f5b018b07a784911eeff8a1e54ff54
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-06-01 23:12:05 +0530

    created api url constant file

---
+++ folder-file-creator/src/constants/api-url.constant.ts
@@ -0,0 +1,10 @@
+export const API_URL = {
+  URL: "http://localhost:3000/folder",
+  URL_For_LIST_OF_FOLDERS: "http://localhost:3000/folder?category=folder",
+  URL_For_LIST_OF_ROOT_FOLDERS:
+    "http://localhost:3000/folder?category=folder&level=root",
+  URL_For_LIST_OF_CHILD_FOLDERS:
+    "http://localhost:3000/folder?category=folder&level=child",
+  URL_For_LIST_OF_SUB_CHILD_FOLDERS:
+    "http://localhost:3000/folder?category=folder&level=sub-child",
+};
