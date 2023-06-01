15121b00bd48bccc9580864db519bbdbb28405b5
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-06-01 23:12:33 +0530

    constant file for common constants

---
+++ folder-file-creator/src/constants/folder.constant.ts
@@ -0,0 +1,15 @@
+export const LEVEL = {
+    ROOT : "root",
+    CHILD : "child",
+    GRAND_CHILD : "grandChild"
+}
+
+export const CATEGORY = {
+    FILE : "file",
+    FOLDER : "folder",
+}
+
+export const NO_DATA = "--";
+export const MESSAGE_NAME_EXIST = 'Name already exist in system';
+export const MESSAGE_NO_FOLDER = "No Folder Exist";
+export const MESSAGE_NO_CHILD_FOLDER = "No Child Folder Exist";
