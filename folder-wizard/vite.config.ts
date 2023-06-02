f49f1d53d4a62c28635cab73cc718da3139c9c76
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-06-02 18:03:38 +0530

    added configuration for build

--- folder-wizard/vite.config.ts
+++ folder-wizard/vite.config.ts
@@ -3,5 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/main.ts',
+      name: 'folder-wizard'
+    }
+  },
   plugins: [svelte()],
 })
